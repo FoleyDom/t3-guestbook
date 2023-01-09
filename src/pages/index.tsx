f3778997af13839679f872fa41135a4fe1384330
Dominique Foley <dfoleydev@gmail.com>
2023-01-09 15:36:12 -0500

    Added styles for the login and logout button.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -82,7 +82,7 @@ const Home = () => {
 		<main className="flex flex-col items-center">
 			<h1 className="text-3xl pt-4">Guestbook</h1>
 			<p>
-				Tutorial for <code>create-t3-app</code>
+				Project using <code>create-t3-app</code>
 			</p>
 
 			<div className="pt-10">
@@ -91,14 +91,26 @@ const Home = () => {
 						<>
 							<p>hi {session.user?.name}</p>
 
-							<button onClick={() => signOut()}>Logout</button>
+							<button
+								type="button"
+								class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+								onClick={() => signOut()}
+							>
+								Logout
+							</button>
 
 							<div className="pt-6">
 								<Form />
 							</div>
 						</>
 					) : (
-						<button onClick={() => signIn('discord')}>Login with Discord</button>
+						<button
+							type="button"
+							class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+							onClick={() => signIn('discord')}
+						>
+							Login with Discord
+						</button>
 					)}
 					<div className="pt-10">
 						<Messages />
